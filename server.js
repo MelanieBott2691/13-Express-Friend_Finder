357d4dc756f8a63591fa892d7bcc31cff604a23e
Melanie Bott <melaniebott@Melanies-iMac.hsd1.ut.comcast.net>
2020-05-24 17:00:02 -0600

    set up bodyParser in server.js to allow express to read the body and parse it into Json object and applied routes

--- server.js
+++ server.js
@@ -5,19 +5,19 @@ var path = require("path");
 var app = express();
 var port = process.env.PORT || 8889;
 
+// allows express to read the body and then parse that into Json object
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json"}));
 
-// routes
+// apply routes
 app.use(express.static("app/public"));
 
 require("./app/routing/apiRouting.js")(app);
 require("./app/routing/apiRouting.js")(app);
 
 app.listen(port, () => console.log("Listening on port %s", port));
-
 
 module.exports = function(app) {
   //code here
