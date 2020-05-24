b886e589f21090701dbe925bbb2cc51e42696216
Melanie Bott <melaniebott@Melanies-iMac.hsd1.ut.comcast.net>
2020-05-24 16:57:26 -0600

    set up server.js and keep getting errors, fixing as I go but now the error says that require(...) is not a function

---
+++ app/data/friends.js
@@ -0,0 +1,32 @@
+var friends = [
+    {
+        name: "Tom",
+        photo: "Image.png",
+        scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+    },
+    {
+        name: "Brady",
+        photo: "Image.png",
+        scores: [3, 2, 6, 4, 5, 1, 2, 5, 4, 1]
+    }
+    // {
+    //     name: "Jan",
+    //     photo: "Image.png",
+    //     scores: [4, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+    // },
+    // {
+    //     name: "Marsha",
+    //     photo: "Image.png",
+    //     scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+    // },
+    // {
+    //     name: "Mike",
+    //     photo: "Image.png",
+    //     scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+    // },
+    // {
+    //     name: "Alice",
+    //     photo: "Image.png",
+    //     scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+    // }
+];
---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+var app = express();
+var port = process.env.PORT || 8889;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json"}));
+
+// routes
+app.use(express.static("app/public"));
+
+require("./app/routing/apiRouting.js")(app);
+require("./app/routing/apiRouting.js")(app);
+
+app.listen(port, () => console.log("Listening on port %s", port));
+
+
+module.exports = function(app) {
+  //code here
+}
