0f226fd286937f14c28604838ea1a9c59f906673
Melanie Bott <melaniebott@Melanies-iMac.hsd1.ut.comcast.net>
2020-05-28 22:35:02 -0600

    Fixed html routing in servers.js and htmlRouting.js

--- server.js
+++ server.js
@@ -1,21 +1,16 @@
 var express = require("express");
-var bodyParser = require("body-parser");
-var path = require("path");
 
 var app = express();
-var port = process.env.PORT || 8889;
-
-// allows express to read the body and then parse that into Json object
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true}));
-app.use(bodyParser.text());
-app.use(bodyParser.json({ type: "application/vnd.api+json"}));
+var port = process.env.PORT || 8080;
 
 // apply routes
 app.use(express.static("app/public"));
+// allows express to read the body and then parse that into Json object
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
 
 require("./app/routing/apiRouting.js")(app);
-require("./app/routing/apiRouting.js")(app);
+require("./app/routing/htmlRouting.js")(app);
 
 app.listen(port, () => console.log("Listening on port %s", port));
 
